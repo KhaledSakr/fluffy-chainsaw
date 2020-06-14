89d1962749b0326b18b465766191507d4741e36c
Khaled Sakr <khaledosamasakr@gmail.com>
2020-06-14 14:21:26 +0200

    Multiple adaptions to allow for multiple items in an invoice

--- index.js
+++ index.js
@@ -10,9 +10,13 @@ const jsreport = require('jsreport-core')(jsreportOptions)
 const fs = require('fs')
 const Handlebars = require('handlebars')
 const moment = require('moment')
-const data = require('./data.json')
+
+const itemAmount = (item) => {
+  return item.rate ? (item.amount * item.rate) : item.amount
+}
 
 jsreport.init().then(async () => {
+  const data = require(process.argv[2] || './data.json')
   const file = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf8')
   const template = Handlebars.compile(file)
   const date = moment()
@@ -20,6 +24,9 @@ jsreport.init().then(async () => {
   const year = date.format('YY')
   const fullYear = date.format('YYYY')
   for (const person of data) {
+    if (person.skip) {
+      continue
+    }
     console.log(`Rendering template for ${person.name}..`)
     const names = person.name.split(' ')
     const initials = `${names[0].charAt(0).toUpperCase()}${names[1].charAt(0).toUpperCase()}`
@@ -28,8 +35,13 @@ jsreport.init().then(async () => {
     const data = {
       ...person,
       invoiceNo,
-      from: person.fromOverride || `1.${month}.${fullYear}`,
-      to: person.toOverride || date.endOf('month').format('DD.MM.YYYY'),
+      items: person.items.map(item => ({
+        ...item,
+        from: item.fromOverride || `1.${month}.${fullYear}`,
+        to: item.toOverride || date.endOf('month').format('DD.MM.YYYY'),
+        amount: itemAmount(item).toFixed(2),
+      })),
+      total: person.items.reduce((sum, item) => sum + itemAmount(item), 0).toFixed(2),
       date: moment().format('DD.MM.YYYY'),
     }
     const content = template(data)
